576a4688097522dccd725a8e8d60abf786e6530a
Gabriel Rodrigues de Moura <gabriel.all@yandex.com>
2018-04-17 20:37:42 -0300

    Melhorando

--- install.py
+++ install.py
@@ -8,22 +8,33 @@ print('O token da atualização é: \n {}\n\n'.format(token))
 
 pkgdistcache = 'XferCommand = /usr/bin/pkgdistcache-client %u %o'  # SrMoura.forAll()['pkgdistcache']
 
-print('Setando configurações\n\n')
-
-os.system('echo "MAKEFLAGS = "-j{}"">>/etc/makepkg.conf'.format(os.cpu_count() + 1))
-os.system('echo "{}" >>/etc/pacman.conf'.format(SrMoura.repo()))
-
-print('Instalando dependencias\n\n')
-pacman.netInstallY('srmoura-keyring pkgdistcache')
+#Instalação das chaves obrigatória
+pacman.netInstallY('srmoura-keyring')
 pacman.populate()
 pacman.updateDB()
-os.system('echo "\n[options]\n{}" >>/etc/pacman.conf'.format(pkgdistcache))
+pacman.optimize()
+
+#configurações para instalações
+D0 = input('Setar configurações agora[s,n]?: ')
+if D0 == 's':
+    print('Setando configurações\n\n')
+    os.system('echo "MAKEFLAGS = "-j{}"">>/etc/makepkg.conf'.format(os.cpu_count() + 1))
+    os.system('echo "{}" >>/etc/pacman.conf'.format(SrMoura.repo()))
+
+D1=input('Deseja instalar o pkgdistcache[s,n]')
+if D1 == 's':
+    print('Instalando dependencias\n\n')
+    pacman.netInstallY('pkgdistcache')
+    pacman.populate()
+    pacman.updateDB()
+    os.system('echo "\n[options]\n{}" >>/etc/pacman.conf'.format(pkgdistcache))
 
 Q0 = input('Deseja otimizar para desktop[s,n]?: ')
 if Q0 == 's':
-    dk='vm.swappiness=1\nvm.vfs_cache_pressure = 50\nvm.dirty_background_bytes = 16777216\nvm.dirty_bytes = 50331648'
-    os.system('echo "{}" >>/etc/sysctl.conf'.format(dk))
-    os.system('echo "{}" >/etc/sysctl.d/99-sysctl.conf'.format(dk))
+    #dk='vm.swappiness=1\nvm.vfs_cache_pressure = 50\nvm.dirty_background_bytes = 16777216\nvm.dirty_bytes = 50331648'
+    #os.system('echo "{}" >>/etc/sysctl.conf'.format(dk))
+    #os.system('echo "{}" >/etc/sysctl.d/99-sysctl.conf'.format(dk))
+    os.system('cp sysctl.conf /etc/sysctl.conf')
     
 Q1 = input('Deseja instalar o yaourt[s,n]?: ')
 if Q1 == 's':
@@ -40,11 +51,11 @@ if Q3 == 's':
 Qck = input('Deseja instalar o kernel-ck[s,n]?: ')
 if Qck == 's':
     print('Configurando repositório, chaves e instalando o linux-ck')
-    os.system('gcc -c -Q -march=native --help=target | grep march')
     os.system('echo "{}" >>/etc/pacman.conf'.format(SrMoura.repoCK()))
     os.system('pacman-key -r 5EE46C4C && pacman-key --sign-key 5EE46C4C')
     pacman.populate()
     pacman.updateDB()
+    os.system('gcc -c -Q -march=native --help=target | grep march')
     ckv = input('Digite o nome printado: ')
     pacman.netInstallY('linux-ck-{} linux-ck-{}-headers'.format(ckv))
 
