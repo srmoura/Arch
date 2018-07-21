36f59e85fd9954dcc3b00ea967b7e7cabfba69c3
Gabriel Rodrigues de Moura <gabriel.all@yandex.com>
2018-07-21 12:44:18 -0300

    Melhorado.

--- SrMoura.py
+++ SrMoura.py
@@ -2,22 +2,29 @@
 """Busca informações e atualizações no servidor SrMoura"""
 import random
 import string
+# from sys import argv as arg
+from configparser import ConfigParser
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 import requests
 
+config = {'file': '/etc/srmoura.conf',  # Arquivo de configuração
+          'api_url': 'https://',  # URL da API
+          'repo': 'https://'  # Repositório
+          }
+
 
 # import psutil
 
-def forToken(token):
+def getToken():
     """Busca informações do Servidor pelo token"""
     url = 'https://srmoura.com.br/api/arch/t{}'
-    data = requests.get(url.format(token)).json()
+    data = requests.get(url.format(read_conf()['token'])).json()
     return data
 
 
-def forAll():
+def getAll():
     """Busca informações genéricas do servidor"""
     url = 'https://srmoura.com.br/api/arch/all'
     data = requests.get(url).json()
@@ -36,7 +43,46 @@ def repoCK():
     return "[repo-ck]\nServer = http://repo-ck.com/\$arch"
 
 
-def send(url='https://srmoura.com.br/api/arch/new_client', data=[]):
+def postSend(data=[], url='https://srmoura.com.br/api/arch/new_client'):
+    """Envia post para o servidor"""
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
     return json
+
+
+def new_client(data):
+    client = postSend(data)
+    write_conf(client['token'])
+    return True
+
+
+def read_conf():
+    conf = ConfigParser()
+    conf.read(config['file'])
+    return {
+        'token': conf.get('srmoura', 'token')
+    }
+
+
+def write_conf(token):
+    conf = ConfigParser()
+
+    conf.read(config['file'])
+
+    conf.add_section('srmoura')
+    conf.set('srmoura', 'token', token)
+    conf.set('srmoura', 'repo', config['repo'])
+
+    with open(config['file'], 'w') as confFile:
+        conf.write(confFile)
+    return True
+
+# def main():
+#    if arg == None:
+#        print("""Uso: SrMoura [OPÇÔES]
+#            - u
+#        """)
+#    elif arg == '-u':
+#        print()
+#    else:
+#        print()
